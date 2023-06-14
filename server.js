129fcd24036dfba17789013688db68f73f2fff32
garlajr <53522156+garlajr@users.noreply.github.com>
2023-06-14 10:06:27 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.get('/', (req, res) => {
     res.status(200).send({ message: 'Ola funciona!!!!' });
 })
 
+app.get('/ola', (req, res) => {
+    res.status(200).send({ message: 'Ola sou a rota ola!!!!' });
+})
+
 app.listen(3001, () =>{
     console.log('Api rodando na porta 3001');
 })
